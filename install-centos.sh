806ec0ac891347c7447025494623a32834905fe5
Jianbo He <hjbxyz@gmail.com>
2018-10-31 16:08:46 +0800

    Update install

--- install-centos.sh
+++ install-centos.sh
@@ -10,23 +10,23 @@ if [ $1 == "lnx64" ]; then
     qmake-qt4
     make
 
-    strip -s rtm-gwas
-    cp rtm-gwas $1/
+    strip -s rtm-gwas-gui
+    mv rtm-gwas-gui $1/
 
 elif [ $1 == "win32" ]; then
 
     i686-w64-mingw32-qmake-qt4
     make release
 
-    i686-w64-mingw32-strip -s release/rtm-gwas.exe
-    cp release/rtm-gwas.exe $1/
+    i686-w64-mingw32-strip -s release/rtm-gwas-gui.exe
+    mv release/rtm-gwas-gui.exe $1/
 
 elif [ $1 == "win64" ]; then
 
     x86_64-w64-mingw32-qmake-qt4
     make release
 
-    x86_64-w64-mingw32-strip -s release/rtm-gwas.exe
-    cp release/rtm-gwas.exe $1/
+    x86_64-w64-mingw32-strip -s release/rtm-gwas-gui.exe
+    mv release/rtm-gwas-gui.exe $1/
 
 fi
