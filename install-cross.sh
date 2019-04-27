38e3204aace4300f758eaf06b53fbc41e9b44a0d
Jianbo He <hjbxyz@gmail.com>
2019-04-27 11:04:11 +0800

    Update install-cross.sh

--- install-cross.sh
+++ install-cross.sh
@@ -1,32 +1,54 @@
 #!/bin/bash
 
+if [[ ! -f /etc/centos-release ]]; then
+    cat /etc/os-release | head -2
+    echo "ERROR: CentOS Linux is required"
+    exit 1
+fi
+
+if [[ $# -ne 1 ]]; then
+    echo "ERROR: invalid argument"
+    echo "./install-cross.sh glnx64|win64|win32"
+    exit 1
+fi
+
 rm -rf $1
-mkdir $1
 
-make distclean
+if [[ -f Makefile ]]; then
+    make distclean
+fi
 
-if [ $1 == "glnx64" ]; then
+if [[ $1 = "glnx64" ]]; then
 
-    qmake-qt4
-    make
+    qmake-qt4 src || exit 1
+    make || exit 1
 
-    strip -s rtm-gwas-gui
-    mv rtm-gwas-gui $1/
+    mkdir $1
+    cp rtm-gwas-gui $1/
+    strip -s $1/rtm-gwas-gui
 
-elif [ $1 == "win32" ]; then
+elif [[ $1 = "win32" ]]; then
 
-    i686-w64-mingw32-qmake-qt4
-    make release
+    mingw32-qmake-qt4 src || exit 1
+    make release || exit 1
 
-    i686-w64-mingw32-strip -s release/rtm-gwas-gui.exe
-    mv release/rtm-gwas-gui.exe $1/
+    mkdir $1
+    cp release/rtm-gwas-gui.exe $1/
+    mingw-strip -s $1/rtm-gwas-gui.exe
 
-elif [ $1 == "win64" ]; then
+elif [[ $1 = "win64" ]]; then
 
-    x86_64-w64-mingw32-qmake-qt4
-    make release
+    mingw64-qmake-qt4 src || exit 1
+    make release || exit 1
 
-    x86_64-w64-mingw32-strip -s release/rtm-gwas-gui.exe
-    mv release/rtm-gwas-gui.exe $1/
+    mkdir $1
+    cp release/rtm-gwas-gui.exe $1/
+    mingw-strip -s $1/rtm-gwas-gui.exe
+
+else
+
+    echo "ERROR: invalid argument: $1"
+    echo "./install-cross.sh glnx64|win64|win32"
+    exit 1
 
 fi
